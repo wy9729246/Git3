4c31cafd3cf1e12f57c869997ea16c40a0eb5153
wanghh <1018875716@qq.com>
2022-07-19 20:56:14 +0800

    Git3

---
+++ src/main/java/com/athaha/git3/Git3.java
@@ -0,0 +1,14 @@
+package com.athaha.git3;
+
+/**
+ * @author WangHHH
+ * @version 1.0
+ * @className Git3
+ * @description
+ * @date 2022/7/19 20:55
+ **/
+public class Git3 {
+    public static void main(String[] args) {
+        System.out.println("Git3");
+    }
+}
